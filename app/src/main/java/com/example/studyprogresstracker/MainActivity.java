6a5bcbbf057a4871c5216945b327c52b0dc4bf95
Celine95 <36287687+Celine95@users.noreply.github.com>
2018-02-11 11:19:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/studyprogresstracker/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.studyprogresstracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    /*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+       //右侧菜单键
+        getMenuInflater().inflate(R.menu.activity_main,menu);
+        return true;
+    }
+    */
+}
