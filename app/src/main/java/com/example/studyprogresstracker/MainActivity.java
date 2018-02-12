56f3468358e03120857f6d48467af24626b68d31
Celine95 <36287687+Celine95@users.noreply.github.com>
2018-02-12 17:20:31 +0800

    update layout xml files

--- app/src/main/java/com/example/studyprogresstracker/MainActivity.java
+++ app/src/main/java/com/example/studyprogresstracker/MainActivity.java
@@ -8,11 +8,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println("hi");
-
-
+        setContentView(R.layout.menu_main);
     }
+
+
     /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
